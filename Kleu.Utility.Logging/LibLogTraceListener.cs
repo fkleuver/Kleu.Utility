640f39d9d330834b8b7a1e5dd0959b661b10d7c9
Fred Kleuver <fred@avurad.nl>
2017-10-06 18:05:12 +0200

    (logging): Add LibLog TraceListener wrapper

---
+++ Kleu.Utility.Logging/LibLogTraceListener.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using Kleu.Utility.Logging.Logging;
+
+namespace Kleu.Utility.Logging
+{
+    public sealed class LibLogTraceListener : TraceListener
+    {
+        private readonly ILog _logger;
+
+        private LibLogTraceListener(ILog logger)
+        {
+            _logger = logger;
+        }
+
+        public static LibLogTraceListener CreateUsingLogger(ILog logger)
+        {
+            if (logger == null)
+                throw new ArgumentNullException(nameof(logger));
+
+            return new LibLogTraceListener(logger);
+        }
+
+        public static LibLogTraceListener CreateUsingLogger(string name)
+        {
+            var logger = LogProvider.GetLogger(name);
+
+            return CreateUsingLogger(logger);
+        }
+
+        public static LibLogTraceListener CreateUsingLogger(Type type, string fallbackTypeName = "System.Object")
+        {
+            var logger = LogProvider.GetLogger(type, fallbackTypeName);
+
+            return CreateUsingLogger(logger);
+        }
+
+        public static LibLogTraceListener CreateUsingCurrentClassLogger()
+        {
+            var logger = LogProvider.GetCurrentClassLogger();
+
+            return CreateUsingLogger(logger);
+        }
+
+        public override void WriteLine(string message)
+        {
+            _logger.Debug(message);
+        }
+
+        public override void Write(string message)
+        { }
+    }
+}
