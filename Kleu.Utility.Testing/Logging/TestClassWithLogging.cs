60518201c6ea2a08f07d72ae46168e851820ed88
Fred Kleuver <fred@avurad.nl>
2017-10-06 19:49:56 +0200

    (testing): Add Logging helpers for test output logging

---
+++ Kleu.Utility.Testing/Logging/TestClassWithLogging.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kleu.Utility.Logging;
+using Xunit.Abstractions;
+
+namespace Kleu.Utility.Testing.Logging
+{
+    /// <summary>
+    /// Base test class that configures LibLog to write to the Xunit test output.
+    /// Must pass <see cref="ITestOutputHelper"/> to the base constructor.
+    /// </summary>
+    public abstract class TestClassWithLogging
+    {
+        protected TestClassWithLogging(ITestOutputHelper output)
+        {
+            LogProvider.SetCurrentLogProvider(new TestOutputLogProvider(output));
+        }
+    }
+}
---
+++ Kleu.Utility.Testing/Logging/TestOutputLogger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Kleu.Utility.Logging;
+using Xunit.Abstractions;
+
+namespace Kleu.Utility.Testing.Logging
+{
+    /// <summary>
+    /// An implementation for LibLog's <see cref="ILog"/> interface that writes all logging output to XUnit's <see cref="ITestOutputHelper"/>, so that log messages can be seen from the test output.
+    /// </summary>
+    public sealed class TestOutputLogger : ILog
+    {
+        private readonly ITestOutputHelper _output;
+
+        public TestOutputLogger(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        public bool Log(LogLevel logLevel, Func<string> messageFunc, Exception exception = null, params object[] formatParameters)
+        {
+            if (messageFunc != null)
+            {
+                var exceptionText = "";
+                if (exception != null)
+                {
+                    exceptionText = $" ({exception.GetType().Name}: {exception.Message})";
+                }
+
+                _output.WriteLine($"[{logLevel}]: {messageFunc()}{exceptionText}");
+
+            }
+            return true;
+        }
+    }
+}
