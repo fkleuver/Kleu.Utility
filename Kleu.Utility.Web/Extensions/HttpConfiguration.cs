a7dc87247568cef806131c5aa45396eae25ccd43
Fred Kleuver <fred@avurad.nl>
2017-10-07 17:05:19 +0200

    (web): Add HttpConfiguration extension for WebApiDiagnostics

---
+++ Kleu.Utility.Web/Extensions/HttpConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using System.Web.Http;
+using Kleu.Utility.Logging;
+
+namespace Kleu.Utility.Web
+{
+    public static class HttpConfigurationExtensions
+    {
+        public static void EnableWebApiDiagnostics(
+            this HttpConfiguration config,
+            bool verbose = true,
+            SourceLevels level = SourceLevels.All,
+            string traceSourceName = "LibLog")
+        {
+            var liblog = new TraceSource(traceSourceName) { Switch = { Level = level } };
+            liblog.Listeners.Add(LibLogTraceListener.CreateUsingCurrentClassLogger());
+
+            var diag = config.EnableSystemDiagnosticsTracing();
+            diag.IsVerbose = verbose;
+            diag.TraceSource = liblog;
+        }
+    }
+}
