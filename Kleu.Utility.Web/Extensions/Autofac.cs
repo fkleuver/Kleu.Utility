3e115b2596a2a41a41a03f95b5b2f20a37c61c71
Fred Kleuver <fred@avurad.nl>
2017-10-07 18:20:06 +0200

    (web): Add ContainerBuilder extension for LogProvider

---
+++ Kleu.Utility.Web/Extensions/Autofac.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using Kleu.Utility.Logging;
+
+namespace Kleu.Utility.Web
+{
+    public static class AutofacExtensions
+    {
+        public static void RegisterLogProvider(this ContainerBuilder builder, string loggerName = "LibLog")
+        {
+            builder.Register(c => LogProvider.GetLogger(loggerName)).AsImplementedInterfaces().InstancePerDependency();
+        }
+    }
+}
