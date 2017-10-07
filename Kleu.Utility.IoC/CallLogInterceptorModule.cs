feb3ca6ac4613b33696192cfe627c4960d4275a2
Fred Kleuver <fred@avurad.nl>
2017-10-07 21:27:38 +0200

    (ioc): Add Autofac logging utilities

---
+++ Kleu.Utility.IoC/CallLogInterceptorModule.cs
@@ -0,0 +1,34 @@
+﻿using Autofac;
+using Castle.DynamicProxy;
+using Kleu.Utility.Logging;
+
+namespace Kleu.Utility.IoC
+{
+    /// <summary>
+    /// Registers the <see cref="CallLogInterceptor"/> so that InterceptWithCallLogger can be used.
+    /// </summary>
+    public sealed class CallLogInterceptorModule : Module
+    {
+        private readonly ILog _logger;
+
+        public CallLogInterceptorModule(ILog logger)
+        {
+            _logger = logger;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+            _logger.Debug($"[Autofac Module] Loading {nameof(CallLogInterceptorModule)}");
+
+            builder
+                .Register(c =>
+                {
+                    var logger = c.Resolve<ILog>();
+                    return new CallLogInterceptor(logger);
+                })
+                .Named<IInterceptor>(nameof(CallLogInterceptor))
+                .InstancePerDependency();
+        }
+    }
+}
---
+++ Kleu.Utility.IoC/DependencyResolutionLogModule.cs
@@ -0,0 +1,62 @@
+﻿using Autofac;
+using Autofac.Core;
+using Kleu.Utility.Common;
+using Kleu.Utility.Logging;
+
+namespace Kleu.Utility.IoC
+{
+    /// <summary>
+    /// An optional module that logs the depency resolution process in a structured format.
+    /// </summary>
+    public sealed class DependencyResolutionLogModule : Module
+    {
+        private readonly ILog _logger;
+
+        private int _depth = 0;
+
+        public DependencyResolutionLogModule(ILog logger)
+        {
+            Guard.AgainstNull(nameof(logger), logger);
+
+            _logger = logger;
+        }
+
+        public DependencyResolutionLogModule() : this(LogProvider.GetCurrentClassLogger())
+        {
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+            _logger.Debug($"[Autofac Module] Loading {nameof(DependencyResolutionLogModule)}");
+        }
+
+        protected override void AttachToComponentRegistration(IComponentRegistry componentRegistry,
+            IComponentRegistration registration)
+        {
+            registration.Preparing += RegistrationOnPreparing;
+            registration.Activating += RegistrationOnActivating;
+            base.AttachToComponentRegistration(componentRegistry, registration);
+        }
+
+        private string GetPrefix()
+        {
+            return new string('-', _depth * 2);
+        }
+
+        private void RegistrationOnPreparing(object sender, PreparingEventArgs preparingEventArgs)
+        {
+            Log($"{GetPrefix()} Resolving  {preparingEventArgs.Component.Activator.LimitType}");
+            _depth++;
+        }
+
+        private void RegistrationOnActivating(object sender, ActivatingEventArgs<object> activatingEventArgs)
+        {
+            _depth--;
+        }
+        private void Log(string message)
+        {
+            _logger.Debug($"[{nameof(DependencyResolutionLogModule)}] {message}");
+        }
+    }
+}
